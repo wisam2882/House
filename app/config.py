4e4651165bd94539fc3d067c88e19874f5dfb848
wisam2882 <wisam_khaleefa@hotmail.com>
2025-01-17 22:05:00 -0700

    config

--- app/config.py
+++ app/config.py
@@ -1,14 +1,17 @@
 import os
 
-
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     FLASK_RUN_PORT = os.environ.get('FLASK_RUN_PORT')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # SQLAlchemy 1.4 no longer supports url strings that start with 'postgres'
-    # (only 'postgresql') but heroku's postgres add-on automatically sets the
-    # url in the hidden config vars to start with postgres.
-    # so the connection uri must be updated here (for production)
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL').replace('postgres://', 'postgresql://')
     SQLALCHEMY_ECHO = True
+
+    # Use SQLite locally if in devcontainer
+    if os.environ.get('DATABASE_URL') == 'sqlite:///dev.db':
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///dev.db'
+    else:
+        # If you later switch to PostgreSQL in other environments (prod), adjust DATABASE_URL
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://')
+
+    # If you have a schema specified, you can use it for the schema
+    SCHEMA = os.environ.get('SCHEMA', 'public')  # Default to 'public' if not set
